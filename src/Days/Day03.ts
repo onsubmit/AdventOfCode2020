09a7a33220247fb780cf3270ae1672cfa0430ac7
sorriso56 <anyoung@live.com>
2022-06-04 22:35:29 -0700

    Add day 3 part 2

--- src/Days/Day03.ts
+++ src/Days/Day03.ts
@@ -24,7 +24,32 @@ export default class Day03 extends Day<boolean[]> {
   };
 
   getPartTwoSolution = (): number => {
-    return 0;
+    const slopes = [
+      { x: 1, y: 1 },
+      { x: 3, y: 1 },
+      { x: 5, y: 1 },
+      { x: 7, y: 1 },
+      { x: 1, y: 2 },
+    ];
+
+    let product = 1;
+    slopes.forEach((slope) => {
+      let x = 0;
+      let numTrees = 0;
+      for (let y = 0; y < this.lines.length; y += slope.y) {
+        const line = this.lines[y];
+        const isTree = line[x];
+        if (isTree) {
+          numTrees++;
+        }
+
+        x = (x + slope.x) % line.length;
+      }
+
+      product *= numTrees;
+    });
+
+    return product;
   };
 }
 
